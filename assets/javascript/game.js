06093c8fca5d2df4e11c24aa8f22f33a30f88a76
Digital Samsquanch <digital.samsquanch@protonmail.com>
2019-08-21 17:13:26 -0400

    fixed the remaining guesses extra guess issue by moving the code into the if statement that subtracts from the remaining guesses. Got the letters guessed to show up properly. Need to make it so keys that are not letters are not counted against the player nor added to the letters used array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,16 @@
-// giving 1 too many chances
-// cannot get the on win event to happen (join does not seem to work?)
-
+/* 
+***To Do List**
+- get the press a button to start feature working
+- after popup window with video happens, change the page in the background to the next puzzle
+    - do this by calling a function? How?
+- organize code into functions
+- add better game over graphics
+*/
 $(document).ready(function() {
 // Create an array of the alphabet
     var alphabet =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    var letterIndex = [];
-    var lettersGuessed = []
-    $("#lettersGuessed").html(lettersGuessed)
+    var lettersGuessed = [];
+    $("#lettersGuessed").html(lettersGuessed);
     var remainingGuesses = 3;
     $("#remainingGuesses").html(remainingGuesses);
     // Create an array of song titles and randomly select them when the game is played
@@ -54,28 +58,28 @@ $(document).ready(function() {
     }
     console.log(question);
     $("#currentWord").html(question);
-    // ***********Generate an element for each letter and space using for loop and jquery
-
-    // document.getElementById("currentWord").textContent = question;
 
 
-    document.onkeyup = function(event) {
-        if(!remainingGuesses == 0){
+    document.onkeydown = function(event) {
+        if(!(remainingGuesses === 0)){
 
             // Determines which key was pressed.
             var userGuess = event.key;
-            console.log(userGuess)
-            console.log(answer.includes(userGuess))
-            // if(!lettersGuessed.includes(userGuess)){
-            //     lettersGuessed.append(userGuess);
-            // }
+            // console.log(userGuess)
+            // console.log(answer.includes(userGuess))
 
-
+            lettersGuessed.push(userGuess);
             if(!answer.includes(userGuess)){
                 remainingGuesses--;
                 $("#remainingGuesses").html(remainingGuesses);
-                lettersGuessed.push(userGuess);
-                console.log("Remaining guesses " + remainingGuesses);
+                $("#lettersGuessed").html(lettersGuessed.join(","));
+                if(remainingGuesses === 0){
+                    $("#currentWordLabel").html("<h1>Game Over</h1>")
+                }
+                // console.log("Remaining guesses " + remainingGuesses);
+                // console.log("Letters guessed " + lettersGuessed.join(","));
+            } else {
+                $("#lettersGuessed").html(lettersGuessed.join(","));
             }
             for(var i = 0; i < answer.length; i++){
                 if(answer[i] === userGuess){
@@ -91,21 +95,6 @@ $(document).ready(function() {
                 console.log(songChoice.url)
                 window.open(songChoice.url,'popUpWindow','height=700,width=1000,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
             }
-
-            
-
-            // If correct the user gets to go again
-            // If user runs out of chances the game ends
-            // if(wrongGuess <= 0){
-            //     // display game over message
-            // }
-            // If user is correct, a window pops up and a youtube video of the song plays
-            /* if(question =  song with spaces added){
-                window.open(songChoice.url, Winner, width:40%, height:auto)
-                // randomly select another song and start the game over
-            } */
-        } else {
-            $("#currentWordLabel").html("<h1>Game Over</h1>")
         }
     }
 });
