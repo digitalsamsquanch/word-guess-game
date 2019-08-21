3511be082ebd420c9b2d27233a2d072b481a28b0
digitalsamsquanch <digital.samsquanch@protonmail.com>
2019-08-20 22:06:51 -0400

    worked on the for loop for adding _ as well as the for loop for checking if the guesses are in the answer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 // Create an array of the alphabet
 var alphabet =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var wrongGuess = 3
+var letterIndex = []
 // Create an array of song titles and randomly select them when the game is played
 var songs = [
     {
@@ -32,35 +33,42 @@ var songs = [
 
 // Choose a random song from the array
 var songChoice = songs[Math.floor(Math.random() * songs.length)];
+var answer = songChoice["name"].toLowerCase()
 
 // Create an empty string as a placeholder
-var question = ""
+var question = []
 // Iterate through the song selected and add an underscore and a space if there is a letter and two spaces if there is just a space
-for(i = 0; i < songChoice["name"].length; i++){
-    if(songChoice["name"][i] === " "){
-        question += "  ";
+for(i = 0; i < answer.length; i++){
+    if(answer[i] === " "){
+        question.push(" ");
     } else {
-    question += ["_ "];
+    question.push("_");
     }
 }
+// ***********Generate an element for each letter and space using for loop and jquery
+
+// document.getElementById("currentWord").textContent = question;
 
 document.onkeyup = function(event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
+    console.log(userGuess)
+    console.log(answer.includes(userGuess))
 
-    if(songChoice["name"].includes(userGuess)){
-        letterIndex = [];
-        for(var i = 0; i < songChoice["name"]; i++){
-            if(songChoice["name"][i] === userGuess){
-                letterIndex.push(i);
-            }
+
+    if(!answer.includes(userGuess)) return wrongGuess--;
+    console.log("Wrong guesses " + wrongGuess)
+
+    for(var i = 0; i < answer; i++){
+        if(answer[i] === userGuess){
+            console.log("Question var " + question[i])
+            console.log("User Guess " + userGuess)
+            question[i] = userGuess;
         }
-    
-    } else {
-        // If incorrect user gets a chance removed
-        wrongGuess--;
+        console.log(question)
     }
+    
 }
 // If correct the user gets to go again
 // If user runs out of chances the game ends
@@ -68,7 +76,7 @@ if(wrongGuess <= 0){
     // display game over message
 }
 // If user is correct, a window pops up and a youtube video of the song plays
-if(question = // song with spaces added){
+/* if(question =  song with spaces added){
     window.open(songChoice.url, Winner, width:40%, height:auto)
     // randomly select another song and start the game over
-}
+} */
